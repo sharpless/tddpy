083deeb7caaf8146cad379472ecb13993982df89
Fredrik Larsson <fredrik@sharpless.se>
2014-05-24 13:13:53 +0200

    Lists now separate for each user

--- lists/tests.py
+++ lists/tests.py
@@ -70,6 +70,12 @@ class ListViewTest(TestCase):
 		self.assertNotContains(response, 'itemye 1')
 		self.assertNotContains(response, 'itemye 2')
 
+	def test_passes_correct_list_to_template(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+		response = self.client.get('/lists/%d/' % (correct_list.id))
+		self.assertEqual(response.context['list'], correct_list)
+
 class NewListTest(TestCase):
 
 	def test_saving_a_POST_request(self):
