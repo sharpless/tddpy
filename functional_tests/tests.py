a838365b75ab573721200c8d0fd3ca6a209bc85c
Fredrik Larsson <fredrik@sharpless.se>
2014-05-25 15:29:32 +0200

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,22 @@
+import sys
 from django.contrib.staticfiles.testing import StaticLiveServerCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(StaticLiveServerCase):
+
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+		super().setUpClass()
+
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super().tearDownClass()
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -17,7 +31,7 @@ class NewVisitorTest(StaticLiveServerCase):
 		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 
 		self.assertIn('To-Do', self.browser.title)
 
@@ -45,7 +59,7 @@ class NewVisitorTest(StaticLiveServerCase):
 		self.browser.quit()
 		self.browser = webdriver.Firefox()
 
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('But peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
@@ -63,7 +77,7 @@ class NewVisitorTest(StaticLiveServerCase):
 		self.assertIn('Buy milk', page_text)
 
 	def test_layout_and_styling(self):
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024, 768)
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
